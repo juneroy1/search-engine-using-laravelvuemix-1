66470d06e037d728ad74ebc886710db4a7e6b114
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2023-05-20 15:19:12 +0800

    feat: add fillable fields for user

--- app/Models/User.php
+++ app/Models/User.php
@@ -19,8 +19,13 @@ class User extends Authenticatable
      */
     protected $fillable = [
         'name',
+        'first_name',
+        'last_name',
+        'middle_name',
         'email',
         'password',
+        'address',
+        'description',
     ];
 
     /**
