6f2302442bef7c1ac1a9d9a1186f41797553d728
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2023-05-20 20:32:40 +0800

    feat: create User Store Request

---
+++ app/Http/Requests/UserStoreRequest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+use Illuminate\Contracts\Validation\Validator;
+class UserStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return [
+            //
+            'first_name' => 'required|max:255',
+            'last_name' => 'required|max:255',
+            'middle_name' => 'required|max:255',
+            'address' => 'required|max:255',
+            'description' => 'required',
+            'email' => 'required|max:255|email|unique:users',
+        ];
+    }
+
+    public function failedValidation(Validator $validator){
+        throw new HttpResponseException(response()->json([
+            'success' => false,
+            'message' => 'Validation Errors',
+            'data'      => $validator->errors()
+        ],403));
+    }
+
+    public function messages(){
+        return [
+            'first_name' => 'First name is required',
+        ];
+    }
+}
