d33522d427aba17acfd766c44ff87c989401c569
Juneroy Quinimon <juneroyquinimon@ocomsoft.com>
2023-05-20 15:24:15 +0800

    feat: refactor changes in FE routes and pages

---
+++ resources/js/pages/users/pages/UserListPage.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>list here</div>
+</template>
+
+<script>
+export default {
+    name: "UserListPage",
+};
+</script>
--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,12 +1,18 @@
 import Animals from "./pages/animals/Animals.vue";
 import Colors from "./pages/colors/Colors.vue";
-import Users from "./pages/users/Users.vue";
+import UserListPage from "./pages/users/pages/UserListPage.vue";
+import UserFormPage from "./pages/users/pages/UserFormPage.vue";
 
 export default [
     {
         path: "/users",
-        name: "users",
-        component: Users,
+        name: "users.list",
+        component: UserListPage,
+    },
+    {
+        path: "/user/create",
+        name: "users.create",
+        component: UserFormPage,
     },
     {
         path: "/animals",
