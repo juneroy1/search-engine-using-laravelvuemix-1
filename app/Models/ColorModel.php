d051c0973652d3c624a307e0fcb7a40ecba310c4
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-21 12:15:22 +0800

    feat: added table pointer for specific table and fillable column

--- app/Models/ColorModel.php
+++ app/Models/ColorModel.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class ColorModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'colors';
+
+    protected $fillable = ['name', 'description', 'creator_id'];
 }
