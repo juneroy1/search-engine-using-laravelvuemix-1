d22dd07ac454de60dc1f5c2a3b091cf8c8ed8f3b
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-21 12:37:05 +0800

    feat: added form for creating color and its function on calling api

--- resources/js/pages/colors/pages/ColorFormPage.vue
+++ resources/js/pages/colors/pages/ColorFormPage.vue
@@ -1,16 +1,51 @@
 <template>
-    <div>color form page</div>
+    <div>
+        <form @submit="registeredNewColor">
+            <div class="container">
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <label for="name">Name:</label>
+                        <input type="text" v-model="item.name" id="name" />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <label for="description">Description:</label>
+                        <input
+                            type="text"
+                            v-model="item.description"
+                            id="description"
+                        />
+                    </div>
+                </div>
+                <div class="row mb-3">
+                    <div class="col-md-12">
+                        <button class="btn btn-primary">Submit</button>
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
 </template>
 <script>
 export default {
     name: "ColorFormPage",
     data() {
-        return {};
+        return {
+            item: {},
+        };
     },
     methods: {
         getOptionColorlist() {
             axios.get("/api/color").then((response) => {
                 console.log("response color", response);
+            });
+        },
+        registeredNewColor(e) {
+            e.preventDefault();
+            console.log("this.item", this.item);
+            axios.post("/api/color", this.item).then((response) => {
+                console.log("response create color", response);
             });
         },
     },
