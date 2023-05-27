99a94dce61eaa93eb3948327058b35b7e824973c
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-27 23:49:55 +0800

    feat: added EloquentColorRepository

--- app/Repositories/EloquentColorRepository.php
+++ app/Repositories/EloquentColorRepository.php
@@ -2,7 +2,30 @@
 
 namespace App\Repositories;
 
-class EloquentColorRepository
+use App\Contracts\Color\ColorRepositoryInterface;
+use App\Models\ColorModel;
+class EloquentColorRepository implements  ColorRepositoryInterface
 {
+    public function create(array $data)
+    {
+        // get all data
+        $data['name'] = $data['first_name'].' '. $data['last_name'];
+        $data['password'] = '';
 
+        // create user
+        return ColorModel::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        $user = ColorModel::findOrFail($id);
+        $user->update($data);
+
+        return $user;
+    }
+
+    public function delete($id)
+    {
+        return ColorModel::destroy($id);
+    }
 }
