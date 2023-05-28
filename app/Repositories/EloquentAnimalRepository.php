575d4d2f262b870d7fc0e8f6751695e23639e62c
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-28 22:56:04 +0800

    feat: added eloquent animal repository

---
+++ app/Repositories/EloquentAnimalRepository.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace App\Repositories;
+
+use App\Contracts\Animal\AnimalRepositoryInterface;
+
+class EloquentAnimalRepository implements AnimalRepositoryInterface {
+
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+    }
+
+    public function update($id, array $data)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+    }
+
+}
