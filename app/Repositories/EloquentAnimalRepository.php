16acd326cfc907e9e670659e5d026a7af800aaee
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-28 22:58:44 +0800

    feat: added function for creating animal repository

--- app/Repositories/EloquentAnimalRepository.php
+++ app/Repositories/EloquentAnimalRepository.php
@@ -4,12 +4,14 @@
 namespace App\Repositories;
 
 use App\Contracts\Animal\AnimalRepositoryInterface;
+use App\Models\AnimalModel;
 
 class EloquentAnimalRepository implements AnimalRepositoryInterface {
 
     public function create(array $data)
     {
         // TODO: Implement create() method.
+        return AnimalModel::create($data);
     }
 
     public function update($id, array $data)
