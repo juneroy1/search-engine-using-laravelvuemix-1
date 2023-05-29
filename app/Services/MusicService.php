3215cef8ab41c3501e8fba74fde79c3894b17b24
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-29 23:52:01 +0800

    feat: added music service

---
+++ app/Services/MusicService.php
@@ -0,0 +1,33 @@
+<?php
+namespace App\Services;
+
+use App\Contracts\UserRepositoryInterface;
+use App\Contracts\UserServiceInterface;
+
+class MusicService implements UserServiceInterface
+{
+    protected  $userRepository;
+
+    public function __construct(UserRepositoryInterface $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
+    public function createUser(array $data)
+    {
+        return $this->userRepository->create($data);
+    }
+
+    public function updateUser($id, array $data)
+    {
+        return $this->userRepository->update($id, $data);
+    }
+
+    public function deleteUser($id)
+    {
+        return $this->userRepository->delete($id);
+    }
+}
+
+
+?>
