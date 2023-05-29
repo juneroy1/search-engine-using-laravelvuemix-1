77e553518f26e5074257a495fc1d8bd10186477d
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-29 23:50:31 +0800

    feat: eloquent music repository

---
+++ app/Repositories/EloquentMusicRepository.php
@@ -0,0 +1,32 @@
+<?php
+
+
+namespace App\Repositories;
+
+use App\Contracts\Animal\AnimalRepositoryInterface;
+use App\Models\AnimalModel;
+
+class EloquentMusicRepository implements AnimalRepositoryInterface {
+
+    public function create(array $data)
+    {
+        // TODO: Implement create() method.
+        return AnimalModel::create($data);
+    }
+
+    public function update($id, array $data)
+    {
+        // TODO: Implement update() method.
+        $animal = AnimalModel::findOrFail($id);
+        $animal->update($data);
+
+        return $animal;
+    }
+
+    public function delete($id)
+    {
+        // TODO: Implement delete() method.
+        return AnimalModel::destroy($id);
+    }
+
+}
