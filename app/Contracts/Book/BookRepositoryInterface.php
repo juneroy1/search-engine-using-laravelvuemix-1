4a0565ca74a8c530e2cf5bd4cc55374eb8e6035c
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-30 23:46:26 +0800

    feat: added function in book repo

--- app/Contracts/Book/BookRepositoryInterface.php
+++ app/Contracts/Book/BookRepositoryInterface.php
@@ -4,5 +4,7 @@
 namespace App\Contracts\Book;
 
 interface BookRepositoryInterface {
-  
+    public function create(array $data);
+    public function update($id, array $data);
+    public function delete($id);
 }
