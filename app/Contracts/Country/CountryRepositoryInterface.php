3c597cad15572839f12693ff9240f7c1fc214568
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-30 23:53:12 +0800

    feat: added function for country repo

--- app/Contracts/Country/CountryRepositoryInterface.php
+++ app/Contracts/Country/CountryRepositoryInterface.php
@@ -1,1 +1,12 @@
 <?php
+
+interface CountryRepositoryInterface {
+
+    public  function createColor(array $data);
+
+    public function updateColor($id, array $data);
+
+    public function deleteColor($id);
+}
+
+?>
