518ad1527488ec7a7afb5e67e66a29909e441969
Juneroy Quinimon <eroyjune@gmail.com>
2023-05-30 23:53:29 +0800

    feat: added country service

--- app/Contracts/Country/CountryServiceInterface.php
+++ app/Contracts/Country/CountryServiceInterface.php
@@ -1,1 +1,9 @@
 <?php
+interface CountryServiceInterface {
+
+    public  function createColor(array $data);
+
+    public function updateColor($id, array $data);
+
+    public function deleteColor($id);
+}
